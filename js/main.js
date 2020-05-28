9b5b0c5010cf76a7f441aa43ff742b375d703cb7
LG <leogourrier@gmail.com>
2020-05-28 17:54:18 +0200

    working on addBook toggle

--- js/main.js
+++ js/main.js
@@ -5,9 +5,34 @@ function stickyHeader() {
 		header.classList.remove('sticky');
 	}
 }
+function toggleAddBtn() {
+	let addBtn = document.querySelector('#addBtn');
+	let theForm = document.querySelector('#theForm');
+	addBtn.addEventListener('click', function() {
+		if (theForm.innerHTML == '') {
+			//if Form doesn't exist, add it
+			//change class to openedForm
+			console.log('Form does not exists');
+			console.log(theForm.classList);
+			console.log('removing closedForm class');
+			console.log('adding openedForm class');
+		} else {
+			//if Form does exist, remove it
+			//change class to closedForm
+			console.log('Form does exists');
+			console.log(theForm.classList.value);
+			console.log('removing openedForm class');
+			console.log('adding closedForm class');
+		}
 
+		console.log('addBook has ended');
+	});
+}
+
+//start the app
 window.onscroll = function() {
 	this.stickyHeader();
 };
 let header = document.getElementById('theHeader');
 let sticky = header.offsetTop;
+toggleAddBtn();
