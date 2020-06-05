91d4ebb15753ab26fad45c06e7b88b8808a8627c
LG <leogourrier@gmail.com>
2020-06-05 15:35:57 +0200

    Responsive Design: Finished

--- js/main.js
+++ js/main.js
@@ -8,21 +8,21 @@ function stickyHeader() {
 function toggleAddBtn() {
 	let addBtn = document.querySelector('#addBtn');
 	let theForm = document.querySelector('#theForm');
+	let submitBtn = document.querySelector('#submitBtn');
+	console.log(submitBtn);
 	addBtn.addEventListener('click', function() {
-		if (theForm.innerHTML == '') {
+		if (theForm.firstChild === null || theForm.firstChild.length == 13) {
 			//if Form doesn't exist, add it
 			//change class to openedForm
-			console.log('Form does not exists');
-			console.log(theForm.classList);
-			console.log('removing closedForm class');
-			console.log('adding openedForm class');
+			theForm.classList.remove('closedForm');
+			theForm.classList.add('openedForm');
+			document.querySelector('#theForm').innerHTML = myForm;
 		} else {
 			//if Form does exist, remove it
 			//change class to closedForm
-			console.log('Form does exists');
-			console.log(theForm.classList.value);
-			console.log('removing openedForm class');
-			console.log('adding closedForm class');
+			theForm.classList.remove('openedForm');
+			theForm.classList.add('closedForm');
+			document.querySelector('#theForm').innerHTML = '';
 		}
 
 		console.log('addBook has ended');
@@ -36,3 +36,33 @@ window.onscroll = function() {
 let header = document.getElementById('theHeader');
 let sticky = header.offsetTop;
 toggleAddBtn();
+let myForm = `
+                <form>
+                    <div id="theTitle">
+                        <label>Title</label>
+                        <input type="text" placeholder="Harry Potter">
+                    </div>
+                    <div id="theAuthor">
+                        <label>Author</label>
+                        <input type="text" placeholder="J.K. Rowling">
+                    </div>
+                    <div id="theYear">
+                        <label>Year</label>
+                        <input type="number" placeholder="1996">
+                    </div>
+                    <div id="thePages">
+                        <label>Pages</label>
+                        <input type="number" placeholder="234">
+                    </div>
+                    <div id="theRead">
+                        <label>Read</label>
+                        <select>
+                            <option>I have NOT read this book</option>
+                            <option>I have read this book</option>
+                        </select>
+                    </div>
+                    <div id="submitDiv">
+                        <button id="submitBtn">Submit Book</button>
+                    </div>
+                </form>
+`;
